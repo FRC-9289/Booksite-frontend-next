00338de0387f8df08065eb5ca8a067823650e912
Aditya-2204 <william1binki@gmail.com>
2025-10-13 16:45:33 -0500

    Added Admin Dashboard

--- src/app/Forms/Admin/Dashboard/Dashboard.tsx
+++ src/app/Forms/Admin/Dashboard/Dashboard.tsx
@@ -2,16 +2,39 @@
 
 import React from "react";
 import styles from './Dashboard.module.css';
+import { useState, useEffect } from "react";
 
 export default function Dashboard() {
+    const [userName, setUserName] = useState<string>("");
+
+    useEffect(() => {
+        const name = localStorage.getItem("userName");
+        if (name) setUserName(name);
+    }, []);
     return (
-        <div>
-            <p>Hello {localStorage.getItem("userName")}</p>
-            <p>Welcome to the Admin Dashboard</p>
-            <div className={styles.buttonContainer}>
-                <button className={styles.button}>Review Forms</button>
-                <button className={styles.button}>Create Forms</button>
+        <>
+            <div className={styles.header}>
+                <h1 className={styles.pHeader}>Hello {userName}</h1>
             </div>
-        </div>
+            <div className={styles.centerContainer}>
+                <div className={styles.buttonContainer}>
+                    <button className={styles.button} onClick={
+                        () => {
+                            window.location.href = '/Forms/Admin/Review';
+                        }
+                    }>Review Forms</button>
+                    <button className={styles.button} onClick={
+                        () => {
+                            window.location.href = '/Forms/Admin/CreateForms';
+                        }
+                    }>Create Forms</button>
+                </div>
+            </div>
+
+            <div>
+                <h2 className={styles.h2}>Notifications</h2>
+                <div className={styles.notificationBar}></div>
+            </div>
+        </>
     )
 }
